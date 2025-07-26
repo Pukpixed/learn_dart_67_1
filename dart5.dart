b7c65ae6d3ca19b529542ee98ab5ed86fb2f3d90
Pukpixed <rmenavijitnavee@gmail.com>
2025-07-26 16:39:48 +0700

    Step 4: Dynamic Variable Type

--- dart5.dart
+++ dart5.dart
@@ -5,7 +5,30 @@ void main() {
 
   // Addition
   int sum = a + b;
-  print(": $sum"); // Output: 15
+  print('Sum: $sum');
 
- 
+  // Subtraction
+  int difference = a - b;
+  print('Difference: $difference');
+
+  // Multiplication
+  int product = a * b;
+  print('Product: $product');
+
+  // Division
+  double quotient = a / b;
+  print('Quotient: $quotient');
+
+  // Modulus
+  int remainder = a % b;
+  print('Remainder: $remainder');
+
+  print("------------------");
+
+  int m = 10;
+
+  // print("++m = ${++m}");
+  // print("m++ = ${m++}");
+  // print("--m = ${--m}");
+  // print("m-- = ${m--}");
 }
