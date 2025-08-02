6fef2d5bde2548eff5a22f12cf1941739bac28b2
Pukpixed <rmenavijitnavee@gmail.com>
2025-08-02 10:34:34 +0700

    Step 9: Function

---
+++ dart9.dart
@@ -0,0 +1,16 @@
+// Function
+void main() {}
+void printMsg(String Joel) {
+  print(getMsg("Joel"));
+  print(add(10, 20));
+}
+
+String getMsg(String name) {
+  return "Hello, $name!";
+}
+
+int add(int a, int b) {
+  return a + b;
+}   
+
+  // This is a simple Dart program that prints "Hello, World!" to the console}
